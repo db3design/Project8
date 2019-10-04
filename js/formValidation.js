cc61453a80c731062d23f7628b02819d0f8d77e9
db3design <51248858+db3design@users.noreply.github.com>
2019-10-04 15:29:28 -0400

    final resubmission

---
+++ js/formValidation.js
@@ -0,0 +1,11 @@
+const name = document.getElementById('userField');
+const message = document.getElementById('messageField');
+
+
+if (name === "") {
+    alert("Please Enter Your Name");
+}
+
+if (message === "") {
+    alert("Please type your message");
+}
